b8d796e47a2c763315678f941aa9a0bd276713c6
scoopd0303 <scott.russell.mcdonald@gmail.com>
2021-12-22 12:13:03 -0600

    set up .env file and got API calls working for a bit. going to branch off and try something else.

--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import querystring from 'querystring'
 import greenSpotifyLogo from '../public/spotify/Spotify_Logo_RGB_Green.png'
 
 export default function Login({genres}) {
@@ -26,24 +27,59 @@ export default function Login({genres}) {
 
           </label>
         </div>
+
+        <div className="border border-ombreNaturelle31/60 shadow-sharp rounded m-4 px-5 py-3">
+          {/* <p>ALBUM: { genres.album.name }</p>
+          <p>ARTIST: { genres.artists[0].name }</p>
+          <p>ISRC: { genres.external_ids.isrc }</p> */}
+        </div>
+
       </div>
     )
 }
 
-export async function getStaticProps() {
+// const getAccessToken = async () => {
 
-  const data = await fetch("https://api.spotify.com/v1/recommendations/available-genre-seeds", 
+//   const client_id = process.env.SPOTIFY_CLIENT_ID;
+//   const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+//   const refresh_token = process.env.SPOTIFY_OAUTH_TOKEN;
+
+//   const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
+//   const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
+
+//   const response = await fetch(TOKEN_ENDPOINT, {
+//     method: 'POST',
+//     headers: {
+//       Authorization: `Basic ${basic}`,
+//       'Content-Type': 'application/x-www-form-urlencoded'
+//     },
+//     body: URLSearchParams.toString({
+//       grant_type: 'refresh_token'
+//     })
+//   })
+
+//   return response.json();
+// }
+
+export const getStaticProps = async () => {
+
+  const { SPOTIFY_OAUTH_TOKEN } = process.env;
+
+  const link1 = 'https://api.spotify.com/v1/tracks/11dFghVXANMlKmJXsNCbNl'
+  const link2 = 'https://api.spotify.com/v1/recommendations/available-genre-seeds'
+
+  const data = await fetch(link1, 
   {
     headers: {
-      Authorization: `Bearer ${process.env.SPOTIFY_OAUTH_TOKEN}`
+      Authorization: `Bearer ${SPOTIFY_OAUTH_TOKEN}`
     }
   }).then(response => response.json());
 
-  console.log(data);
+  console.log(data)
 
   return {
     props: {
-      genres: ["test", "test2"]
+      genres: data
     }
   }
 }
